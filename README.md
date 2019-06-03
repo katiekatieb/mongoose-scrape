e1b209d78cd20c43187d72662aea063c64db6841
katie brasfield <katiebrasfield@gmail.com>
2019-06-02 22:56:07 -0400

    fix comments

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # mongoose
+
+## Contributors
+@katiekatieb
+
+
+## Technology
+* Node, Mongo, Mongoose
+* Live demo: https://immense-garden-35813.herokuapp.com/
+
+## License 
+* This project is licensed under The MIT License (MIT).
+
+## How-to use this code
+* Fork the repo and add your content.
+
+## Contributing Guidelines
+All contributions and suggestions are welcome!
+For direct contributions, please fork the repository and file a pull request. 
+
+## Contact
+* e-mail: katiebrasfield@gmail.com
--- server.js
+++ server.js
@@ -64,19 +64,17 @@ app.get("/articles/:id", function(req, res) {
   db.Article.findOne({ _id: req.params.id })
     .populate("comment")
     .then(function(dbArticle) {
-      res.render("article", {articles: dbArticle});
+      console.log(dbArticle)
+      res.render("article", {article: dbArticle});
     })
     .catch(function(err) {
       res.json(err);
     });
 });
 
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id/comments", function(req, res) {
   db.Comment.create(req.body)
     .then(function(dbComment) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
     })
     .then(function(dbArticle) {
